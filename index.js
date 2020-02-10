efeb467c56d82425d82b97b3f2ae4d74a5a836bf
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-02-10 23:02:28 +0100

    chore: migrate to proton v2 and linux

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { render, App } from "proton-native"; // import the proton-native components
-import Main from "./container/Main";
+import Main from "./src/container/Main";
 
 const options = {
-  title: "Razer Chroma Audio Visualizer!",
-  size: {
-    w: 300,
-    h: 300
-  },
-  menuBar: false
+	title: "Razer Chroma Audio Visualizer!",
+	size: {
+		w: 300,
+		h: 300
+	},
+	menuBar: false
 };
 
 const AudioVizualizer = () => (
-  <App>
-    <Main {...options} />
-  </App>
+	<App>
+		<Main {...options} />
+	</App>
 );
 
 render(<AudioVizualizer />);
